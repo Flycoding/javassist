b8ebf04bae393222e6a43f990600e0cc5fc43cc7
flycoding <Flycoding@yeah.net>
2013-07-10 10:13:17 +0800

    $1,$2,$3... is updatable.

--- src/test/java/com/flyingh/javassist/Demo3.java
+++ src/test/java/com/flyingh/javassist/Demo3.java
@@ -1,6 +1,8 @@
 package com.flyingh.javassist;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -41,11 +43,18 @@ public class Demo3 {
 
 	@Test
 	public void test() throws NotFoundException, CannotCompileException,
-			IOException {
+			IOException, InstantiationException, IllegalAccessException,
+			SecurityException, NoSuchMethodException, IllegalArgumentException,
+			InvocationTargetException {
 		CtClass ctClass = ClassPool.getDefault().get(
 				"com.flyingh.javassist.Point");
 		CtMethod ctMethod = ctClass.getDeclaredMethod("move");
-		ctMethod.insertBefore("{System.out.println($1);System.out.println($2);}");
+		ctMethod.insertBefore("{System.out.println($1);System.out.println($2);$1=100;System.out.println($1);$1=250;System.out.println($1);}");
 		ctClass.writeFile();
+		Class<?> cls = ctClass.toClass();
+		Object newInstance = cls.newInstance();
+		Method declaredMethod = cls.getDeclaredMethod("move", int.class,
+				int.class);
+		declaredMethod.invoke(newInstance, 3, 5);
 	}
 }
